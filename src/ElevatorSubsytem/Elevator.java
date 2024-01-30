c050b4707ae91c522c280a315257aac9a3b1ee52
Victoria Malouf <victoriamalouf@cmail.carleton.ca>
2024-01-30 18:07:09 -0500

    method headers

--- src/ElevatorSubsytem/Elevator.java
+++ src/ElevatorSubsytem/Elevator.java
@@ -1,11 +1,42 @@
 package ElevatorSubsytem;
 
-public class Elevator implements Runnable {
-    public Elevator() {
+import SchedulerSubsystem.Scheduler;
+import SchedulerSubsystem.SchedulerEvent;
 
+public class Elevator implements Runnable {
+    /** Single floor travel time */
+    public final double TRAVEL_TIME = 8.5;
+    private int currentFloor;
+    private Scheduler scheduler;
+
+    public Elevator() {
+        this.currentFloor = 0;
     }
 
-    public void arrived(int floor){
+    /**
+     * Returns the boarding or deboarding time given the numberOfPassengers
+     *
+     * @param numberOfPassengers Amount of passengers boarding/deboarding the elevator.
+     * @return Time in seconds
+     */
+    private double getBoardingTime(int numberOfPassengers) {
+        return 1.84 * numberOfPassengers + 6.79;
+    }
+
+    /**
+     * Elevator travels to a specified floor.
+     * @param floorNumber
+     */
+    public void travelToFloor(int floorNumber) {
+        // Requires use of TRAVEL_TIME
+    }
+
+    /**
+     * Elevator tells the scheduler that it has arrived at a floor
+     *
+     * @param schedulerEvent
+     */
+    public void sendArrivedAtFloorMessage(SchedulerEvent schedulerEvent){
 
     }
     @Override
---
+++ src/Utlities/Passenger.java
@@ -0,0 +1,13 @@
+package Utlities;
+import SchedulerSubsystem.SchedulerEvent;
+
+/**
+ * Passenger holds all data related to a passenger
+ * @param arrivalTime Time the passenger arrives at the floor.
+ * @param sourceFloor Floor that the passenger requests an elevator from.
+ * @param direction The direction a passenger would like to go (UP/DOWN).
+ * @param destinationFloor Floor that the passenger would like to arrive at.
+ */
+public record Passenger(int arrivalTime, int sourceFloor, SchedulerEvent.Direction direction, int destinationFloor) {
+
+}
