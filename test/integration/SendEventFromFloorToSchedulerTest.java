1c9825553a7e6758a8a5ba8b45eac423ffe6d7a2
Braeden Kloke <braedenkloke@gmail.com>
2024-02-01 13:49:12 -0500

    test(integration): add test for sending events from floor to scheduler

---
+++ test/integration/SendEventFromFloorToSchedulerTest.java
@@ -0,0 +1,13 @@
+package integration;
+
+import org.junit.jupiter.api.*;
+
+/**
+ * Tests Floor sending an event to Scheduler.
+ */
+public class SendEventFromFloorToSchedulerTest {
+
+    @Test
+    void sendEventFromFloorToSchedulerTest() {
+    }
+}
