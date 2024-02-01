05c7122163d8a18ca03fe13fb720db0798f2caeb
Braeden Kloke <braedenkloke@gmail.com>
2024-02-01 13:52:34 -0500

    test(system): add one passenger test

---
+++ test/system/OnePassengerTest.java
@@ -0,0 +1,14 @@
+package system;
+
+import org.junit.jupiter.api.*;
+
+/**
+ * Tests simulation for one passenger.
+ */
+public class OnePassengerTest {
+
+    @Test
+    void sampleTest() {
+
+    }
+}
