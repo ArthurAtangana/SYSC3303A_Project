bd3feb04678661d4de06e739d26ccfc6d01216a5
Victoria Malouf <victoriamalouf@cmail.carleton.ca>
2024-02-01 22:16:32 -0500

    feat(Floor): send event from floor to scheduler

--- src/FloorSubsystem/Floor.java
+++ src/FloorSubsystem/Floor.java
@@ -19,6 +19,30 @@ public class Floor implements Runnable {
         transmitterToScheduler.setDestinationReceiver(schedulerReceiver);
     }
 
+    @Override
+    public void run() {
+        while (true) {
+            sendFloorButtonPressedEvent();
+        }
+    }
+
+    /**
+     * Sets passenger list
+     *
+     * @param passengers
+     */
+    public void setPassengers(ArrayList<Passenger> passengers) {
+        this.passengers = passengers;
+    }
+
+    /**
+     * Returns list of passengers.
+     * @return passengers
+     */
+    public ArrayList<Passenger> getPassengers() {
+        return passengers;
+    }
+
     /**
      * Receives notification from scheduler that the elevator floor has changed.
      */
@@ -28,18 +52,31 @@ public class Floor implements Runnable {
 
     /**
      * Setting the lamp to display which floor the elevator is on.
+     *
      * @param floorNumber The floor number that the elevator is currently on.
      */
     public void setLamp(int floorNumber) {
-
+        floorLamp = floorNumber;
     }
 
     /**
      * Sends floor button request to scheduler.
+     *
      * @param systemEvent
      */
     public void sendPassengerRequestToScheduler(ElevatorSystemEvent systemEvent) {
         this.transmitterToScheduler.send(systemEvent);
+    }
+
+    /**
+     * If passenger has arrived at the floor, send floor button pressed event to the scheduler.
+     */
+    public void sendFloorButtonPressedEvent() {
+        if (passengerHasArrived()) {
+            Passenger p = passengers.remove(0);
+            FloorButtonPressedEvent e = new FloorButtonPressedEvent(p);
+            sendPassengerRequestToScheduler(e);
+        }
     }
 
     /**
@@ -54,20 +91,6 @@ public class Floor implements Runnable {
         // Stubbed method
         // TODO if simulation time > arrival time of first passenger in passengers return true
         return true;
-    }
-
-    @Override
-    public void run() {
-        while (true) {
-            // see if simulation time > arrival time
-            if (passengerHasArrived()) {
-                Passenger p = passengers.remove(0);
-                FloorButtonPressedEvent e = new FloorButtonPressedEvent(p);
-                sendPassengerRequestToScheduler(e);
-            }
-
-        }
-
     }
 
 }
--- test/unit/FloorSubsystem/FloorTest.java
+++ test/unit/FloorSubsystem/FloorTest.java
@@ -6,8 +6,9 @@ import Networking.Events.ElevatorSystemEvent;
 import Networking.Events.FloorButtonPressedEvent;
 import Networking.Events.Passenger;
 import Networking.Receivers.DMA_Receiver;
-import SchedulerSubsystem.Scheduler;
 import org.junit.jupiter.api.*;
+
+import java.util.ArrayList;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
@@ -17,19 +18,42 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
  */
 public class FloorTest {
 
+    DMA_Receiver receiver;
+    Floor f;
+    ArrayList<Passenger> passengers;
+    Passenger p1;
+
+    @BeforeEach
+    void setUp() {
+        receiver = new DMA_Receiver();
+        f = new Floor(1, receiver);
+        passengers = new ArrayList<>();
+        p1 = new Passenger(5, 1, Direction.UP, 3);
+        passengers.add(p1);
+    }
+
     @Test
     void sendPassengerRequestToSchedulerTest() {
         // Arrange
-        DMA_Receiver receiver = new DMA_Receiver();
-        Floor f = new Floor(1, receiver);
-        Passenger passenger = new Passenger(5, 1, Direction.UP, 3);
-        ElevatorSystemEvent event = new FloorButtonPressedEvent(passenger);
+        ElevatorSystemEvent event = new FloorButtonPressedEvent(p1);
 
         // Act
         f.sendPassengerRequestToScheduler(event);
 
         // Assert
         assertEquals(event, receiver.receive());
+    }
+
+    @Test
+    void removeArrivedPassengersTest() {
+        // Arrange
+        f.setPassengers(passengers);
+
+        // Act
+        f.sendFloorButtonPressedEvent();
+
+        // Assert
+        assertTrue(f.getPassengers().isEmpty());
     }
 
 
