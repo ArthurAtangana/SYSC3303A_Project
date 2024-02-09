692c4810d462b65b8ed8067df04256ed58a5d44a
Braeden Kloke <braedenkloke@gmail.com>
2024-02-08 19:34:18 -0500

    test(scheduler): initialize unit tests for scheduler

---
+++ test/unit/SchedulerSubsystem/SchedulerTest.java
@@ -0,0 +1,18 @@
+package unit.SchedulerSubsystem;
+
+import org.junit.jupiter.api.*;
+
+/**
+ * Unit tests for class Scheduler.
+ */
+public class SchedulerTest {
+
+    @Test
+    public void isElevatorStoppingTest() {
+        // Arrange
+
+        // Act
+
+        // Assert
+    }
+}
