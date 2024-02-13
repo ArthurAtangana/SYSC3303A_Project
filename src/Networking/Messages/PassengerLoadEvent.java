2b92d84c87d3c77463f5867a680e34aafcbcf89d
orselane <77144245+orselane@users.noreply.github.com>
2024-02-13 13:06:45 -0500

    Added Javadoc to PassengerLoadEvent (#47)
    * Added Javadoc to PassengerLoadEvent
    
    * Added an "intended flow" amendment to the docs.

--- src/Networking/Messages/PassengerLoadEvent.java
+++ src/Networking/Messages/PassengerLoadEvent.java
@@ -2,5 +2,13 @@ package Networking.Messages;
 
 import java.util.ArrayList;
 
+/**
+ * PassengerLoadEvent record, holds a list of passengers to load onto an elevator.
+ * Intended flow: Floor -> Scheduler(Loader).
+ *
+ * @param passengers The passengers to be loaded on the elevator
+ * @author Alexandre Marques
+ * @version Iteration-2
+ */
 public record PassengerLoadEvent(ArrayList<DestinationEvent> passengers) implements SystemEvent {
 }
