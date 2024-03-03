20762bbfe5d5b903d3ed4d623e460429f132a876
Arthur Atangana <arthur.atangana@protonmail.com>
2024-03-03 14:04:34 -0500

    Add state pattern library

---
+++ src/StatePatternLib/Context.java
@@ -0,0 +1,29 @@
+package StatePatternLib;
+
+public abstract class Context implements Runnable {
+    // FIXME: If there's a way to "late bind" the State generic type, that would be ideal...
+    //  In the mean time, the typing in State should suffice to enforce correct State types in Context classes.
+    private State curState;
+    // TODO in Concrete state: add persistent state info
+
+    public Context() {
+        setState(null); // TODO in Concrete State: Replace by start state
+    }
+
+    public void setState(State newState) {
+        curState = newState;
+    }
+
+    @Override
+    public void run() {
+        // Start next state if not null (null == end state)
+        while (curState != null)
+            curState.startState();
+    }
+
+    // TODO in Concrete State: Add application logic, package private methods to be called by state activities,
+    //  and/or when selecting next state.
+
+    // Note: Can implement MVC by having an "UpdateView" triggered on exit in abstract "State";
+    //  Send a message with relevant state packaged for view to process and show.
+}
---
+++ src/StatePatternLib/State.java
@@ -0,0 +1,58 @@
+package StatePatternLib;
+
+
+public abstract class State<ContextT extends Context> {
+    protected final ContextT context;
+
+    public State(ContextT context) {
+        this.context = context;
+    }
+
+    /**
+     * Method called on state entry to "start" the state execution.
+     * Includes all execution parts of a state, including:
+     * 1. OnEntry
+     * 2. doActivity
+     * 3. newStateSelection (based on events during doActivity)
+     * 4. OnExit
+     * 5. updating context to new state
+     */
+    public void startState() {
+        onEntry();
+        doActivity(); // The state has no memory, only the context
+        State<ContextT> newState = selectNextState();
+        onExit();
+        context.setState(newState); // Set next state
+    }
+
+    /**
+     * Code which is triggered on state entry.
+     */
+    protected void onEntry() {
+    }
+
+
+    /**
+     * Code which is triggered on state exit.
+     */
+    protected void onExit() {
+    }
+
+
+    /**
+     * Code which is executed until state change.
+     * <p>
+     * doActivity needs an exit condition by either:
+     * - Implementing an event detection mechanism inside.
+     * - Having a defined start/exit point independent of external inputs.
+     */
+    protected void doActivity() {
+    }
+
+    /**
+     * Algorithm to select next state, can execute event dependant exit code.
+     *
+     * @return The next state.
+     */
+    protected abstract State<ContextT> selectNextState();
+}
