18181b57befebabfe3afce2c60924c2fd16483ab
Arthur Atangana <arthur.atangana@protonmail.com>
2024-03-03 14:05:23 -0500

    Add SubsystemContext

---
+++ src/Subsystem/SubsystemContext.java
@@ -0,0 +1,33 @@
+package Subsystem;
+
+import Configuration.Config;
+import Configuration.Configurator;
+import Messaging.Receivers.DMA_Receiver;
+import Messaging.Receivers.Receiver;
+import Messaging.SystemMessage;
+import StatePatternLib.Context;
+
+public abstract class SubsystemContext extends Context {
+
+    // Static config loaded and shared across all subsystems
+    private static final String JSON_FILEPATH = "res/system-config-00.json";
+    protected static final Config CONFIG = (new Configurator(JSON_FILEPATH).getConfig());
+
+    // Fields common in each subsystem
+    protected final Receiver rx;
+    protected final int key;
+
+    protected SubsystemContext(int key) {
+        super();
+        this.rx = new DMA_Receiver(key); // TODO: Replace by UDP receiver later
+        this.key = key;
+    }
+
+    public Receiver getRx() {
+        return rx;
+    }
+
+    public SystemMessage receive() {
+        return rx.receive();
+    }
+}
