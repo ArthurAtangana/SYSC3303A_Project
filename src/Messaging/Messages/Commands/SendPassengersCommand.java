370bab56682afe9a73ae93f912c089ca7e684f61
orselane <77144245+orselane@users.noreply.github.com>
2024-03-05 13:07:08 -0500

    Develop/transceiver factories (#67)
    * Adding TransceiverFacotries (DMA, UDP).
    
    - Added new constructor in ReceiverUDP for wildcard port (for clients)
    - Both factories automatically bind the clients to the server on transmitter creation. The server still needs to bind during runtime.
    - Server receiver implemented as singleton, lets us have a "known" address/reference.
    
    * Deprecated loader class... not replaced yet!
    
    - I think it led to confusion and increased complexity.
    - There should be a way to get similar benefits while staying strictly in the scheduler.
    - Staying in the scheduler gives us better control over the receiver address.
    
    * Made use of TransceiverFactories in main.
    
    - Refactored Subsystems to use the abstract transceivers rather than DMA.
    - Floor now stores a static transmitter to all created floors (DMA, always. For the dispatcher)
    
    * Creating ReceiverComposite to allow for multiple receivers to have a combined super queue.
    
    * Activating ReceiverUDP by starting them in factory directly.
    
    * Re-Implemented loader through floor and scheduler.
    
    - Added transmitter to floor
    - Added new event handling to scheduler
    - Modified related messages
    - TODO: Binding in scheduler
    
    * Moving subsystems into their own package.
    
    - Adding a subsystem interface to be replaced later. For now used for polymorphic behavior.
    
    * Adding scheduler binding on subsystem construction.
    
    - Added bind message on Floor, Elevator at end of constructor
    - Bind message is generic by taking in subsystem class types to identify sender
    - Scheduler handles new event in process message
    - TODO: generics are broken for the factory, committing now for a baseline while figuring things out.
    
    * Fixing generic issues for late binding transmitters.
    
    - Transmitters can now attempt to bind to any receiver, but they will ignore receivers of the wrong type.
    - This does mean there's tighter coupling in the concrete transmitter constructors in a sense, but I think this is a good solution.
    
    * Deleting loader, now that its been fully replaced.
    
    * fix: change variable FROM server_receiver TO serverReceiver
    
    Incorrect syntax in variable name. Should be camelCase not snake_case.
    
    ---------
    
    Co-authored-by: Alexandre Marques <alex.marques@live.ca>
    Co-authored-by: Braeden Kloke <braedenkloke@gmail.com>

--- src/Messaging/Messages/Commands/SendPassengersCommand.java
+++ src/Messaging/Messages/Commands/SendPassengersCommand.java
@@ -1,21 +1,17 @@
 package Messaging.Messages.Commands;
 
 import Messaging.Messages.Direction;
-import Messaging.Transceivers.Transmitters.TransmitterDMA;
-
-import java.io.Serializable;
 
 /**
  * SendPassengersCommand Record, Command to floor to send passengers back through the provided transmitter.
  * Passing a transmitter allows for dynamic creation of receivers on the scheduler end.
  *
  * @param floorNum The floor number the command is addressed to.
- * @param tx The transmitter through which the passengers should be sent.
  *
  * @author Alexandre Marques
  * @version Iteration-2
  */
-public record SendPassengersCommand(int floorNum, Direction dir, TransmitterDMA tx) implements SystemCommand {
+public record SendPassengersCommand(int floorNum, int elevNum, Direction dir) implements SystemCommand {
     /**
      * Match floor number with the given key.
      * @param key The floor number of the floor trying to process this event.
