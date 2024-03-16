c1e716c5d318409032f492d81b1ad4451a98713b
Alexandre Marques <alex.marques@live.ca>
2024-03-16 18:39:45 -0400

    Making MAX_MSG_SIZE static in receivers.

--- src/Messaging/Transceivers/Receivers/ReceiverUDP.java
+++ src/Messaging/Transceivers/Receivers/ReceiverUDP.java
@@ -15,7 +15,7 @@ import java.net.SocketException;
  * @version Iteration-3
  */
 public class ReceiverUDP extends ReceiverUDPProxy implements Runnable {
-    public final int MAX_MSG_SIZE = 9001; // Reduce to save on memory (may cause crash), Increase if EOFException is raised.
+    public static final int MAX_MSG_SIZE = 9001; // Reduce to save on memory (may cause crash), Increase if EOFException is raised.
     private final DatagramSocket receiveSocket;
     private int lastSequenceReceived;
 
